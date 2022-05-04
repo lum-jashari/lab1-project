9c105938baa52a7d0120064b0da1a6217190f791
lum <45062873+lum-jashari@users.noreply.github.com>
2022-05-04 18:52:47 +0200

    Add files via upload

---
+++ User1Edit.cs
@@ -0,0 +1,73 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TravelAgency1.Models;
+namespace TravelAgency1.Controllers
+{
+    public class User1Edit : Controller
+    {
+        private readonly TravelAgencyContext _context;
+
+        public User1Edit(TravelAgencyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user1 = await _context.User1s.FindAsync(id);
+            if (user1 == null)
+            {
+                return NotFound();
+            }
+            return View(user1);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Firstname,Lastname,Age,Country,Address,Email,Password")] User1 user1)
+        {
+            if (id != user1.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user1);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!User1Exists(user1.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user1);
+        }
+
+        private bool User1Exists(int id)
+        {
+            return _context.User1s.Any(e => e.Id == id);
+        }
+    }
+}
