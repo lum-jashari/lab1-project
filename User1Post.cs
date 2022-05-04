b20566059bb0833179a1a4c8e7e08145ac74514f
altidaberisha <93524843+altidaberisha@users.noreply.github.com>
2022-05-04 18:41:49 +0200

    Add files via upload

---
+++ User1Post.cs
@@ -0,0 +1,35 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TravelAgency1.Models;
+
+namespace TravelAgency1.Controllers
+{
+    public class User1Post : Controller
+    {
+        private readonly TravelAgencyContext _context;
+
+        public User1Post(TravelAgencyContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Firstname,Lastname,Age,Country,Address,Email,Password")] User1 user1)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user1);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user1);
+        }
+    }
+}
